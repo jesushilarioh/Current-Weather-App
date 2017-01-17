c2d81aa8d88038d735ba9a3fed93ff5e41656a2c
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-01-17 14:16:29 -0600

    modified and added to index html and js files.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var ourRequest = new XMLHttpRequest(); // Create a new instance
+var cityContainer = document.getElementById('city');
+var conditionContainer = document.getElementById('condition');
+var tempContainer = document.getElementById('temp');
 
 // GET request from ip-api
 ourRequest.open('GET', 'http://ip-api.com/json');
@@ -22,11 +25,32 @@ ourRequest.onload = function() {
         var condition = weatherData.weather[0].main;
         var temp = weatherData.main.temp;
         console.log("The condition is " + condition + ".");
+        renderHTMLCondition(condition);
         console.log("The temperature is " + temp + " kelvin.");
+        renderHTMLTemp(temp);
     };
     // Send GET request to open weather map
     weatherRequest.send();
+
     console.log("I live in " + city + ".");
+    renderHTMLCity(city);
 };
 // Send GET request to ip-api
 ourRequest.send();
+
+// Render HTML
+function renderHTMLCity(data) {
+    var htmlString = " ";
+    htmlString += data;
+    cityContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+function renderHTMLCondition(data) {
+    var htmlString = " ";
+    htmlString += data;
+    conditionContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+function renderHTMLTemp(data) {
+    var htmlString = " ";
+    htmlString += data;
+    tempContainer.insertAdjacentHTML('beforeend', htmlString);
+}

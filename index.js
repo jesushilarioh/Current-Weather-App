2472c6956e1afa822aa2fecd3178a038310930ee
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-02-01 21:14:26 -0600

    modified files.

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
-var ourRequest = new XMLHttpRequest(); // Create a new instance
+var request = new XMLHttpRequest(); // Create a new instance
 var cityContainer = document.getElementById('city');
 var conditionContainer = document.getElementById('condition');
 var tempContainer = document.getElementById('temp');
 var iconContainer = document.getElementById('icon');
 
 // GET request from ip-api
-ourRequest.open('GET', 'http://ip-api.com/json');
+request.open('GET', 'http://ip-api.com/json');
 
 // Function to load on initial page load
-ourRequest.onload = function() {
+request.onload = function() {
     // Local variables
-    var ourData = JSON.parse(ourRequest.responseText);
+    var ourData = JSON.parse(request.responseText);
     var lat = ourData.lat;
     var lon = ourData.lon;
     var city = ourData.city;
-    var weatherRequest = new XMLHttpRequest(); // Create another instance
 
     // GET request from open weather map
-    weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=f62651bb11bb663233a4e55634a22266');
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=f62651bb11bb663233a4e55634a22266');
 
     // Function to load on initial page load
-    weatherRequest.onload = function(){
+    request.onload = function(){
         // Local variable
-        var weatherData = JSON.parse(weatherRequest.responseText);
+        var weatherData = JSON.parse(request.responseText);
         var condition = weatherData.weather[0].main;
         var temp = weatherData.main.temp;
         var icon = weatherData.wind.speed;
         temp = ((temp-273) * (9/5) + 32).toFixed(1);
-        renderHTMLCondition(condition);
-        renderHTMLTemp(temp);
-        renderHTMLIcon(icon);
+        renderHTML(conditionContainer, condition);
+        renderHTML(tempContainer, temp);
+        renderHTML(iconContainer, icon);
     };
     // Send GET request to open weather map
-    weatherRequest.send();
-    renderHTMLCity(city);
+    request.send();
+    renderHTML(cityContainer, city);
 };
 // Send GET request to ip-api
-ourRequest.send();
+request.send();
 
 // Render HTML
-function renderHTMLCity(data) {
+function renderHTML(info, data) {
     var htmlString = " ";
     htmlString += data;
-    cityContainer.insertAdjacentHTML('beforeend', htmlString);
-}
-function renderHTMLCondition(data) {
-    var htmlString = " ";
-    htmlString += data;
-    conditionContainer.insertAdjacentHTML('beforeend', htmlString);
-}
-function renderHTMLTemp(data) {
-    var htmlString = " ";
-    htmlString += data;
-    tempContainer.insertAdjacentHTML('beforeend', htmlString);
-}
-function renderHTMLIcon(data) {
-    var htmlString = " ";
-    htmlString += "Wind speed is " + data + " mph.";
-    iconContainer.insertAdjacentHTML('beforeend', htmlString);
+
+    if (info === conditionContainer || info === tempContainer || info === cityContainer) {
+      info.insertAdjacentHTML('beforeend', htmlString);
+    } else {
+      info.insertAdjacentHTML('beforeend', "The winds speed is " + htmlString + " mph.")
+    }
 }

becb86c4a5916cb87b7bf90b511a6b0dc9019685
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-02-06 22:00:46 -0600

    modified index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 (function() {
   // Global Variables
   const request = new XMLHttpRequest(), // Create a new instance
-        cityContainer = document.getElementById('city'),
-        countryContainer = document.getElementById('country'),
-        conditionContainer = document.getElementById('condition'),
         tempContainer = document.getElementById('temp'),
-        windContainer = document.getElementById('wind'),
-        C2FButton = document.getElementById("CtoF"),
-        imgList = document.getElementById('imgList');
-  var tempChange = document.getElementsByClassName('fahrenheit');
+        cityContainer = document.getElementById('city'),
+        conditionContainer = document.getElementById('condition'),
+        countryContainer = document.getElementById('country'),
+        windContainer = document.getElementById('wind');
 
   // GET request from ip-api
   request.open('GET', 'http://ip-api.com/json');
@@ -29,11 +26,14 @@
       request.onload = function(){
           // Local variable
           const weatherData = JSON.parse(request.responseText),
+                C2FButton = document.getElementById("CtoF"),
+                imgList = document.getElementById('imgList'),
                 condition = weatherData.weather,
                 temp = weatherData.main.temp,
                 wind = weatherData.wind.speed,
                 temp1 = ((temp-273) * (9/5) + 32).toFixed(1),
                 temp2 = (temp - 273.15).toFixed(1);
+            var tempChange = document.getElementsByClassName('fahrenheit');
 
           // Display info onto index.html
           renderHTML(conditionContainer, condition);
@@ -97,12 +97,12 @@
           break;
         // Write to element with #imgList id
         case imgList:
-        for (var i = 0; i < data.length; i++) {
+          for (var i = 0; i < data.length; i++) {
             let imgNode = document.createElement("IMG");
             info.appendChild(imgNode);
             info.children[i].setAttribute("id", "img" + i);
             info.children[i].src = "http://openweathermap.org/img/w/" + data[i].icon + ".png";
-        }
+          }
           break;
         // In case there is an error upon loading.
         default:

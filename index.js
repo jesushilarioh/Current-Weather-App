5b1c305fb31a7f59cfcd45ca0bffa6be175f0638
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-08-14 18:05:05 -0500

    refactored some and created variables.

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ if ("geolocation" in navigator || navigator.geolocation) {
     
 function success(position) {
     let longitude = position.coords.longitude,
-        latitude  = position.coords.latitude;
-        // params = 'longitude=' + longitude + '&latitude=' + latitude;
+        latitude  = position.coords.latitude,
+        params = 'longitude=' + longitude + '&latitude=' + latitude,
+        requestURL = './connection/location.php?' + params;
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', './connection/location.php?latitude=' + latitude + '&longitude=' + longitude, true);
+    xhr.open('GET', requestURL, true);
     xhr.onloadstart = function() {
         console.log('xhr started');
     }
@@ -21,8 +22,22 @@ function success(position) {
     }
     xhr.onload = function() {
         if (this.status == 200) {
-            let results = JSON.parse(this.responseText);
-            console.log(results);
+            let results = JSON.parse(this.responseText),
+                city = results.city,
+                cityInfo = {
+                    name: city.name,
+                    country: city.country,
+                    population: city.population,
+                    timezone: city.timezone,
+                    sunrise: new Date(city.sunrise * 1000),
+                    sunset: new Date(city.sunset * 1000)
+                },
+                forecast = results.list.slice();
+                
+            console.log("Forcast", forecast);
+
+        } else {
+            console.log('Error: ', this.statusText);
         }
     }
     xhr.onloadend = function() {
